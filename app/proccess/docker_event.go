335b8dfed67ca342cbc53a8702e467526fea8931
melodycchen <melodycchen@tencent.com>
2022-12-01 12:17:02 +0800

    获取容器事件

--- app/proccess/docker_event.go
+++ app/proccess/docker_event.go
@@ -1,13 +1,14 @@
 package proccess
 
 import (
-	"fmt"
 	"github.com/docker/docker/api/types"
 	"github.com/docker/docker/api/types/events"
 	"log"
 	"strings"
 	"time"
 )
+
+var ReLoadContainerInfo = make(chan bool, 3)
 
 type DockerEvent struct {
 	*DockerApi
@@ -20,6 +21,7 @@ func NewDockerEvent(api *DockerApi) *DockerEvent {
 	return &DockerEvent{DockerApi: api}
 }
 
+// Handler 处理函数
 func (d *DockerEvent) Handler() {
 	log.Println("开始处理 docker event")
 
@@ -30,37 +32,51 @@ func (d *DockerEvent) Handler() {
 	for {
 		select {
 		case message := <-messages:
-			d.message(&message)
+			result := d.message(&message)
+			if result {
+				ReLoadContainerInfo <- true
+			}
 		case err := <-errs:
 			log.Fatalf("docker event error:%s", err)
 		}
-		//log.Println("开始休息")
-		time.Sleep(100 * time.Millisecond)
+		time.Sleep(500 * time.Millisecond)
 	}
 }
 
-func (d *DockerEvent) message(message *events.Message) {
-
+// message 判定是否有容器变动信息
+//  @param message
+//  @return bool
+func (d *DockerEvent) message(message *events.Message) bool {
+	if message.Type != "container" {
+		log.Printf("过滤掉不是 container event：type:%s, action:%s", message.Type, message.Action)
+		return false
+	}
 	if strings.HasPrefix(message.Action, "exec_create") ||
 		strings.HasPrefix(message.Action, "exec_die") ||
 		strings.HasPrefix(message.Action, "exec_start") {
-		return
+		log.Printf("过滤掉event：type:%s, action:%s", message.Type, message.Action)
+		return false
 	}
-	fmt.Println(message.Status)
-	fmt.Println(message.Scope)
-	fmt.Println(message.Actor)
-	fmt.Println(message.From)
 	switch message.Action {
-	case "create":
-		break
+	case "kill":
+		fallthrough
 	case "die":
-		break
-	case "disconnect":
-		return
+		fallthrough
+	case "start":
+		fallthrough
+	case "stop":
+		fallthrough
+	case "destroy":
+		fallthrough
+	case "create":
+		log.Printf("----------> 触发 event ：action:%s", message.Action)
+		return true
+	case "attach":
+		fallthrough
+	case "resize":
+		return false
 	default:
 		log.Fatalf("docker event other message waiting handle: %s", message.Action)
 	}
-
-	fmt.Println(23242)
-
+	return false
 }
