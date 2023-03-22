cec3eedc14c51c12c2b31e0928e05e94fa029991
melodycchen <melodycchen@tencent.com>
2023-03-22 17:47:17 +0800

    添加vue

--- router/router.go
+++ router/router.go
@@ -4,6 +4,7 @@ import (
 	"github.com/gin-gonic/gin"
 	"github.com/melodywen/docker-trace-log/app/http/middleware"
 	"github.com/melodywen/docker-trace-log/contracts"
+	"net/http"
 )
 
 // RouteLoad 加载路由
@@ -12,6 +13,12 @@ import (
 func RouteLoad(router *gin.Engine, app contracts.AppAttributeInterface) error {
 
 	router.Use(middleware.LogMiddleWare(app))
+
+	router.Static("/public", "./public")
+
+	router.GET("/", func(c *gin.Context) {
+		c.Redirect(http.StatusFound, "/public/index.html")
+	})
 
 	router.GET("/ping", func(c *gin.Context) {
 		c.JSON(200, gin.H{
