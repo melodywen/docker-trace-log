58584ba74b3ef566e616e57c8135cd43212201d9
melodycchen <melodycchen@tencent.com>
2023-03-22 20:05:11 +0800

    提交代码

--- resources/log-web/src/App.vue
+++ resources/log-web/src/App.vue
@@ -1,85 +1,88 @@
-<script setup>
-import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
-    </div>
-  </header>
-
-  <RouterView />
+  <a-layout has-sider>
+    <a-layout-sider
+      v-model:collapsed="collapsed" :trigger="null" collapsible>
+      <div class="logo" />
+      <a-menu v-model:selectedKeys="selectedKeys" theme="dark" mode="inline">
+        <a-menu-item key="1">
+          <user-outlined />
+          <span class="nav-text">nav 1</span>
+        </a-menu-item>
+        <a-menu-item key="2">
+          <video-camera-outlined />
+          <span class="nav-text">nav 2</span>
+        </a-menu-item>
+        <a-menu-item key="3">
+          <upload-outlined />
+          <span class="nav-text">nav 3</span>
+        </a-menu-item>
+        <a-menu-item key="4">
+          <bar-chart-outlined />
+          <span class="nav-text">nav 4</span>
+        </a-menu-item>
+        <a-menu-item key="5">
+          <cloud-outlined />
+          <span class="nav-text">nav 5</span>
+        </a-menu-item>
+        <a-menu-item key="6">
+          <appstore-outlined />
+          <span class="nav-text">nav 6</span>
+        </a-menu-item>
+        <a-menu-item key="7">
+          <team-outlined />
+          <span class="nav-text">nav 7</span>
+        </a-menu-item>
+        <a-menu-item key="8">
+          <shop-outlined />
+          <span class="nav-text">nav 8</span>
+        </a-menu-item>
+      </a-menu>
+    </a-layout-sider>
+    <a-layout >
+      <a-layout-header :style="{ background: '#fff', padding: 0 }">
+        <menu-unfold-outlined v-if="collapsed" class="trigger" @click="() => (collapsed = !collapsed)" />
+        <menu-fold-outlined v-else class="trigger" @click="() => (collapsed = !collapsed)" />
+      </a-layout-header>
+      <a-layout-content :style="{ margin: '10px 5px 0', overflow: 'initial' }">
+        <div   :style="{ margin: '5px', padding: '10px', background: '#fff', minHeight: '840px' }">
+          ...
+          <br />
+          Really
+          <br />
+          <menu-unfold-outlined />
+          ...
+          <br />
+          ...
+          <br />
+          content
+        </div>
+      </a-layout-content>
+      <a-layout-footer :style="{ textAlign: 'center' }"> </a-layout-footer>
+    </a-layout>
+  </a-layout>
 </template>
-
-<style scoped>
-header {
-  line-height: 1.5;
-  max-height: 100vh;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
-}
-
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
-}
-</style>
+<script>
+import { UserOutlined, VideoCameraOutlined, UploadOutlined, BarChartOutlined, CloudOutlined, AppstoreOutlined, TeamOutlined, ShopOutlined, MenuUnfoldOutlined, MenuFoldOutlined } from '@ant-design/icons-vue';
+import { defineComponent, ref } from 'vue';
+export default defineComponent({
+  components: {
+    UserOutlined,
+    VideoCameraOutlined,
+    UploadOutlined,
+    BarChartOutlined,
+    CloudOutlined,
+    AppstoreOutlined,
+    TeamOutlined,
+    ShopOutlined,
+    MenuUnfoldOutlined,
+    MenuFoldOutlined
+  },
+  setup() {
+    return {
+      selectedKeys: ref(['4']),
+      collapsed: ref(false),
+    };
+  },
+});
+</script>
+<style></style>
